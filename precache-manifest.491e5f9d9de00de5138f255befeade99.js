b3a208c26ab4243959caace13f694eca9e93eb78
Sanix2802 <47119344+Sanix2802@users.noreply.github.com>
2019-06-27 22:36:45 +0300

    Add files via upload

---
+++ precache-manifest.491e5f9d9de00de5138f255befeade99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a97f488a38e9972b5a2b",
+    "url": "/static/js/main.a97f488a.chunk.js"
+  },
+  {
+    "revision": "8ada04d5c3a282ce5389",
+    "url": "/static/js/2.8ada04d5.chunk.js"
+  },
+  {
+    "revision": "a97f488a38e9972b5a2b",
+    "url": "/static/css/main.8650de56.chunk.css"
+  },
+  {
+    "revision": "8ada04d5c3a282ce5389",
+    "url": "/static/css/2.76a98b78.chunk.css"
+  },
+  {
+    "revision": "5fbfa99fcbe60df7c2ec323cfb0593cc",
+    "url": "/index.html"
+  }
+];
